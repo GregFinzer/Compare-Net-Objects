3c05c7aae0a14b16962334ed8f3df8901e5c978c
gfinzer <gfinzer@hotmail.com>
2016-03-04 21:39:38 -0500

    .NET Automated Builds V3.05

--- Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
+++ Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.04.0.0")]
-[assembly: AssemblyFileVersion("3.04.0.0")]
+[assembly: AssemblyVersion("3.05.0.0")]
+[assembly: AssemblyFileVersion("3.05.0.0")]
--- Compare-NET-Objects/Properties/AssemblyInfo.cs
+++ Compare-NET-Objects/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.04.0.0")]
-[assembly: AssemblyFileVersion("3.04.0.0")]
+[assembly: AssemblyVersion("3.05.0.0")]
+[assembly: AssemblyFileVersion("3.05.0.0")]
 [assembly: CLSCompliant(true)]
