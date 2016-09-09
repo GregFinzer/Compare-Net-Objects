f1e8727b563789f3dc712fff82002e4bbaf36c2f
IainBapty <iain@bapty.org>
2016-09-09 23:45:22 +0100

    Compare DataColumn Details
    Added some comparison tests around DataColumn. Did not appear necessary
    to add a specific 'DataColumnComparer' implementation.
    
    Updated DataTableComparer to compare column details in addition to
    column count.

---
+++ Compare-NET-Objects-Tests/CompareDataColumnTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Data;
+using KellermanSoftware.CompareNetObjects;
+using NUnit.Framework;
+
+namespace KellermanSoftware.CompareNetObjectsTests
+{
+    [TestFixture]
+    public class CompareDataColumnTests
+    {
+        #region Class Variables
+        private CompareLogic _compare;
+        #endregion
+
+        #region Setup/Teardown
+
+        /// <summary>
+        /// Code that is run once for a suite of tests
+        /// </summary>
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+
+        }
+
+        /// <summary>
+        /// Code that is run once after a suite of tests has finished executing
+        /// </summary>
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+
+        }
+
+        /// <summary>
+        /// Code that is run before each test
+        /// </summary>
+        [SetUp]
+        public void Initialize()
+        {
+            _compare = new CompareLogic();
+        }
+
+        /// <summary>
+        /// Code that is run after each test
+        /// </summary>
+        [TearDown]
+        public void Cleanup()
+        {
+            _compare = null;
+        }
+        #endregion
+
+        #region Tests
+        [Test]
+        public void DataColumnPositiveTest()
+        {
+            var dc1 = new DataColumn("Flavor", typeof(string));
+            var dc2 = new DataColumn("Flavor", typeof(string));
+            Assert.IsTrue(_compare.Compare(dc1, dc2).AreEqual);
+        }
+
+        [Test]
+        public void DataColumnMismatchedNameTest()
+        {
+            var dc1 = new DataColumn("Flavor", typeof(string));
+            var dc2 = new DataColumn("Flavour", typeof(string));
+            var result = _compare.Compare(dc1, dc2);
+            Assert.IsFalse(result.AreEqual);
+
+            Assert.AreEqual(1, result.Differences.Count);
+
+            Assert.AreEqual("Flavor", result.Differences[0].Object1Value);
+            Assert.AreEqual("Flavour", result.Differences[0].Object2Value);
+        }
+
+        [Test]
+        public void DataColumnMismatchedTypeTest()
+        {
+            var dc1 = new DataColumn("Price", typeof(string));
+            var dc2 = new DataColumn("Price", typeof(decimal));
+            var result = _compare.Compare(dc1, dc2);
+            Assert.IsFalse(result.AreEqual);
+
+            Assert.AreEqual(1, result.Differences.Count);
+
+            Assert.AreEqual("System.String", result.Differences[0].Object1Value);
+            Assert.AreEqual("System.Decimal", result.Differences[0].Object2Value);
+        }
+        #endregion
+    }
+}
