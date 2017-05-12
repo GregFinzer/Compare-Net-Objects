a401b98b69e17f58e7affce3da6dd37c253a34f8
gfinzer <gfinzer@hotmail.com>
2017-05-11 20:59:11 -0400

    Comment class with Tuple

--- Compare-NET-Objects-Tests/TestClasses/ClassWithTuple.cs
+++ Compare-NET-Objects-Tests/TestClasses/ClassWithTuple.cs
@@ -7,6 +7,6 @@ namespace KellermanSoftware.CompareNetObjectsTests.TestClasses
 {
     public class ClassWithTuple
     {
-        public List<(int i, string s)> L { get; } = new List<(int i, string s)>();
+        //public List<(int i, string s)> L { get; } = new List<(int i, string s)>();
     }
 }
