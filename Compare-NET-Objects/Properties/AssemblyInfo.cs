8d58b2d4118c2a5771a7b1f765725367225154b0
Greg Finzer <gfinzer@hotmail.com>
2017-07-15 07:26:08 -0400

    Change to 3.09

--- Compare-NET-Objects/Properties/AssemblyInfo.cs
+++ Compare-NET-Objects/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.08.0.0")]
-[assembly: AssemblyFileVersion("3.08.0.0")]
+[assembly: AssemblyVersion("3.09.0.0")]
+[assembly: AssemblyFileVersion("3.09.0.0")]
 [assembly: CLSCompliant(true)]
