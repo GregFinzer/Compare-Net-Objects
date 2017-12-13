21766f92f6eb8a1829524f8f97e193a3d8d15d6e
Greg Finzer <gfinzer@hotmail.com>
2017-12-12 22:10:47 -0500

    Version 4.01

--- Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
+++ Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.00.0.0")]
-[assembly: AssemblyFileVersion("4.00.0.0")]
+[assembly: AssemblyVersion("4.01.0.0")]
+[assembly: AssemblyFileVersion("4.01.0.0")]
