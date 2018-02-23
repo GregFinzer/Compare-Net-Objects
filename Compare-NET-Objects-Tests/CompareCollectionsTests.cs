d52862ff3c45d04dc3932ff610e7f75ed4bd05e2
Mark Kharitonov <mark.kharitonov@gmail.com>
2018-02-23 12:34:47 -0500

    Add CollectionComparer to compare collections of different types when IgnoreObjectTypes is set.

---
+++ Compare-NET-Objects-Tests/CompareCollectionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using KellermanSoftware.CompareNetObjects;
+using NUnit.Framework;
+
+namespace KellermanSoftware.CompareNetObjectsTests
+{
+    [TestFixture]
+    public class CompareCollectionsTests
+    {
+        [Test]
+        public void CollectionsSame()
+        {
+            var coll1 = new List<KeyValuePair<string, byte[]>>
+            {
+                new KeyValuePair<string, byte[]>("Hello", new byte[] { 1, 2, 3 })
+            };
+            var coll2 = new Dictionary<string, byte[]>
+            {
+                {"Hello", new byte[] { 1, 2, 3 } }
+            };
+            var compareLogic = new CompareLogic
+            {
+                Config = new ComparisonConfig
+                {
+                    IgnoreObjectTypes = true,
+                    IgnoreCollectionOrder = true
+                }
+            };
+
+            Assert.IsTrue(compareLogic.Compare(coll1, coll2).AreEqual);
+        }
+    }
+}
