9ccdf1f9877db88de950ced424f094cddf9161e4
Jan Dvořák <jandvorak.public@gmail.com>
2018-02-27 07:14:34 +0100

    Added support for System.Collections.Immutable.ImmutableArray without adding reference to main library

---
+++ Compare-NET-Objects-Tests/CompareImmutableArrayTests.cs
@@ -0,0 +1,86 @@
+﻿using KellermanSoftware.CompareNetObjects;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace KellermanSoftware.CompareNetObjectsTests
+{
+    using System;
+
+    [TestFixture]
+    public class CompareImmutableArrayTests
+    {
+        [Test]
+        public void CompareSameArrays()
+        {
+            var comparer = new CompareLogic();
+            var a1 = new[] { 1, 2, 3, 4, 6 }.ToImmutableArray();
+            var a2 = new[] { 1, 2, 3, 5, 6 }.ToImmutableArray();
+
+            var result = comparer.Compare(a1, a2);
+
+            Assert.IsFalse(result.AreEqual);
+        }
+
+        [Test]
+        public void CompareDiferentArrays()
+        {
+            var comparer = new CompareLogic();
+            var a1 = new[] { 1, 2, 3 }.ToImmutableArray();
+            var a2 = new[] { 1, 2, 3 }.ToImmutableArray();
+
+            var result = comparer.Compare(a1, a2);
+
+            if (!result.AreEqual)
+                throw new Exception(result.DifferencesString);
+        }
+
+        [Test]
+        public void CompareArraysOfDiferentOrder()
+        {
+            var comparer = new CompareLogic { Config = new ComparisonConfig { IgnoreCollectionOrder = true } };
+            var a1 = new[] { 1, 2 }.ToImmutableArray();
+            var a2 = new[] { 2, 1 }.ToImmutableArray();
+
+            var result = comparer.Compare(a1, a2);
+
+            if (!result.AreEqual)
+                throw new Exception(result.DifferencesString);
+        }
+
+        [Test]
+        public void CompareSameArraysOnLevel2()
+        {
+            var comparer = new CompareLogic();
+            var e1 = new TestEnvelope(new[] { "John", "Jane" });
+            var e2 = new TestEnvelope(new[] { "John", "Jane" });
+
+            var result = comparer.Compare(e1, e2);
+
+            if (!result.AreEqual)
+                throw new Exception(result.DifferencesString);
+        }
+
+        [Test]
+        public void CompareDiferentArraysOnLevel2()
+        {
+            var comparer = new CompareLogic();
+            var e1 = new TestEnvelope(new[] { "John", "Jane" });
+            var e2 = new TestEnvelope(new[] { "John", "Mary" });
+
+            var result = comparer.Compare(e1, e2);
+
+            Assert.IsFalse(result.AreEqual);
+        }
+    }
+
+    public class TestEnvelope
+    {
+        public TestEnvelope(IEnumerable<string> names)
+        {
+            Names = names.ToImmutableArray();
+        }
+
+        public ImmutableArray<string> Names { get; }
+    }
+}
---
+++ Compare-NET-Objects/TypeComparers/ImmutableArrayComparer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace KellermanSoftware.CompareNetObjects.TypeComparers
+{
+    /// <summary>
+    /// Compares System.Collections.Immutable.ImmutableArray
+    /// </summary>
+    public class ImmutableArrayComparer : BaseTypeComparer
+    {
+        private readonly ListComparer _listComparer;
+
+        /// <summary>
+        /// Constructor that takes a root comparer
+        /// </summary>
+        /// <param name="rootComparer"></param>
+        public ImmutableArrayComparer(RootComparer rootComparer) : base(rootComparer)
+        {
+            _listComparer = new ListComparer(rootComparer);
+        }
+
+        /// <summary>
+        /// Returns true if both are immutable arrays with same generic argument type
+        /// </summary>
+        /// <param name="type1">The type of the first object</param>
+        /// <param name="type2">The type of the second object</param>
+        /// <returns></returns>
+        public override bool IsTypeMatch(Type type1, Type type2)
+        {
+            return TypeHelper.IsImmutableArray(type1) && TypeHelper.IsImmutableArray(type2);
+        }
+
+        /// <summary>
+        /// Compares two immutable arrays
+        /// </summary>
+        public override void CompareType(CompareParms parms)
+        {
+            //This should never happen, null check happens one level up
+            if (parms.Object1 == null || parms.Object2 == null)
+                return;
+
+            parms.Object1 = ((IList) parms.Object1).Cast<object>().ToArray();
+            parms.Object2 = ((IList) parms.Object2).Cast<object>().ToArray();
+
+            _listComparer.CompareType(parms);
+        }
+    }
+}
