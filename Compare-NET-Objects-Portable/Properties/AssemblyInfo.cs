b520a51c130865135164034412dd08f83965254d
gfinzer <gfinzer@hotmail.com>
2018-06-30 14:52:18 -0400

    NET Automated Builds Version 4.54.0.0

--- Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
+++ Compare-NET-Objects-Portable/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.53.0.0")]
-[assembly: AssemblyFileVersion("4.53.0.0")]
+[assembly: AssemblyVersion("4.54.0.0")]
+[assembly: AssemblyFileVersion("4.54.0.0")]
