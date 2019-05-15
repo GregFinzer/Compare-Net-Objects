70dd130944a03b3108f221304eab9f1808e8710d
Ali Alwan <ali.alwan@atrium-uw.com>
2019-05-15 16:06:54 +0100

    failing test to illustrate issue #143 added

--- Compare-NET-Objects-Tests/IgnoreTests.cs
+++ Compare-NET-Objects-Tests/IgnoreTests.cs
@@ -1,5 +1,8 @@
-﻿using KellermanSoftware.CompareNetObjects;
+﻿using System.Dynamic;
+using KellermanSoftware.CompareNetObjects;
 using KellermanSoftware.CompareNetObjectsTests.TestClasses;
+using KellermanSoftware.CompareNetObjectsTests.TestClasses.IgnoreExample;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace KellermanSoftware.CompareNetObjectsTests
@@ -13,14 +16,55 @@ namespace KellermanSoftware.CompareNetObjectsTests
             ComparisonConfig config = new ComparisonConfig();
             config.MembersToIgnore.Add("Person.Name");
 
-            Person person1 = new Person() {Name="Darth Vader"};
-            Person person2 = new Person() { Name = "Anakin Skywalker" };
+            Person person1 = new Person() {Name = "Darth Vader"};
+            Person person2 = new Person() {Name = "Anakin Skywalker"};
 
             CompareLogic compare = new CompareLogic(config);
 
-            var result = compare.Compare(person1,person2);
+            var result = compare.Compare(person1, person2);
             Assert.IsTrue(result.AreEqual);
+        }
 
+        [Test]
+        public void ExpandoObject()
+        {
+            DynamicContainer a = new DynamicContainer()
+            {
+                expando = JsonConvert.DeserializeObject<ExpandoObject>("{\"test\": \"1\"}")
+            };
+
+            DynamicContainer b = new DynamicContainer()
+            {
+                expando = new ExpandoObject()
+            };
+
+            CompareLogic compareLogic = new CompareLogic();
+            compareLogic.Config.MembersToIgnore.Add("*test*");
+            compareLogic.Config.MembersToIgnore.Add("*expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("*.expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("*DynamicContainer.expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("*.DynamicContainer.expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("test*");
+            compareLogic.Config.MembersToIgnore.Add("expando.test*");
+            compareLogic.Config.MembersToIgnore.Add(".expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("DynamicContainer.expando.test*");
+            compareLogic.Config.MembersToIgnore.Add(".DynamicContainer.expando.test*");
+            compareLogic.Config.MembersToIgnore.Add("*test");
+            compareLogic.Config.MembersToIgnore.Add("*expando.test");
+            compareLogic.Config.MembersToIgnore.Add("*.expando.test");
+            compareLogic.Config.MembersToIgnore.Add("*DynamicContainer.expando.test");
+            compareLogic.Config.MembersToIgnore.Add("*.DynamicContainer.expando.test");
+            compareLogic.Config.MembersToIgnore.Add("test");
+            compareLogic.Config.MembersToIgnore.Add("expando.test");
+            compareLogic.Config.MembersToIgnore.Add(".expando.test");
+            compareLogic.Config.MembersToIgnore.Add("DynamicContainer.expando.test");
+            compareLogic.Config.MembersToIgnore.Add(".DynamicContainer.expando.test");
+            compareLogic.Config.CustomComparers.Add(new StringMightBeFloat(RootComparerFactory.GetRootComparer()));
+            compareLogic.Config.MaxDifferences = 50;
+
+            ComparisonResult result = compareLogic.Compare(a, b);
+
+            Assert.IsTrue(result.AreEqual);
         }
     }
 }
---
+++ Compare-NET-Objects-Tests/TestClasses/IgnoreExample/StringMightBeFloat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using KellermanSoftware.CompareNetObjects;
+using KellermanSoftware.CompareNetObjects.TypeComparers;
+
+namespace KellermanSoftware.CompareNetObjectsTests.TestClasses.IgnoreExample
+{
+    class StringMightBeFloat : BaseTypeComparer
+    {
+        public StringMightBeFloat(RootComparer rootComparer) : base(rootComparer)
+        {
+        }
+
+        public override bool IsTypeMatch(Type type1, Type type2)
+        {
+            return type1 == typeof(string) && type2 == typeof(string);
+        }
+
+        public override void CompareType(CompareParms parms)
+        {
+            {
+
+                var st1 = (string)parms.Object1;
+                var st2 = (string)parms.Object2;
+
+                if (st1.Equals(st2))
+                {
+                    return;
+                }
+
+                float flt1;
+                float flt2;
+
+                if (float.TryParse(st1, out flt1) && float.TryParse(st2, out flt2))
+                {
+                    if (flt1.Equals(flt2))
+                    {
+                        return;
+                    }
+                }
+
+                AddDifference(parms);
+            }
+        }
+    }
+}
