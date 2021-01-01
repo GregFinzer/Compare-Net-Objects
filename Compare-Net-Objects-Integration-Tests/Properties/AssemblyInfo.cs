41ecf53af6a781b144894ed64739666a8ca74f79
Greg <gfinzer@hotmail.com>
2021-01-01 10:46:14 -0500

    Copyright changed to 2021

--- Compare-Net-Objects-Integration-Tests/Properties/AssemblyInfo.cs
+++ Compare-Net-Objects-Integration-Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kellerman Software")]
 [assembly: AssemblyProduct("Compare-Net-Objects-Integration-Tests")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
