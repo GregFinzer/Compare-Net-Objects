defd804c5fc02a729bc100e72b1c1b73d0b09bbb
Victor Irzak <victor.irzak@zomp.com>
2023-09-10 09:40:08 -0400

    Add test cases and adjust for NET46

--- Compare-NET-Objects/TypeBackwardsCompatibilityExtensions.cs
+++ Compare-NET-Objects/TypeBackwardsCompatibilityExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace KellermanSoftware.CompareNetObjects
 {
-#if !NETSTANDARD && !NET
+#if !NETSTANDARD && !NET && !NET46
     /// <summary>
     /// Extensions for Type to provide backward compatibility between latest and older .net Framework APIs.
     /// </summary>
