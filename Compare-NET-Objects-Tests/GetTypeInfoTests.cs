769ff5bcc009070c235318724ba55948feb8314e
Victor Irzak <victor.irzak@zomp.com>
2023-09-10 09:49:19 -0400

    Add test case

---
+++ Compare-NET-Objects-Tests/GetTypeInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using KellermanSoftware.CompareNetObjects;
+using KellermanSoftware.CompareNetObjectsTests.Attributes;
+using KellermanSoftware.CompareNetObjectsTests.TestClasses;
+using NUnit.Framework;
+
+namespace KellermanSoftware.CompareNetObjectsTests
+{
+    [TestFixture]
+    public class GetTypeInfoTests
+    {
+        [Test]
+        public void EnsureCallingGetTypeInfoCompiles()
+        {
+            _ = typeof(AlPeTests).GetTypeInfo();
+        }
+    }
+}
